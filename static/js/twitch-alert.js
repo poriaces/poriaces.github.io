5ca5fcbc673f5bd3221ded45aa23eadadb487601
ComSecNinja <timo@comsec.ninja>
2017-05-28 18:42:02 +0300

    CSS fixes

---
+++ static/js/twitch-alert.js
@@ -0,0 +1,23 @@
+var streamersUpdate = function() {
+    var key = "ph06xyfig2wp3z4qzz2mv6y49caq6un";
+    var users = ["MrHamsterOW", "r0n5k1", "TipiOW", "tombhaonair"];
+    $(".twitch-alert").empty();
+    users.forEach(function (user) {
+        twitchInfo(user, key, function(data, err) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            if (data.stream) {
+                var se = $(".streamer[data-template]").clone();
+                se.removeAttr("data-template");
+                se.attr("href", "https://twitch.tv/"+user);
+                se.find("img").attr("src", data.channel.logo);
+                se.find("span").html("<b>"+data.channel.display_name+"</b> is streaming <b>"+data.stream.game+"</b>!");
+                $(".twitch-alert").append(se);
+            }
+        });
+    });
+};
+setInterval(streamersUpdate, 1000*60*2);
+streamersUpdate();
