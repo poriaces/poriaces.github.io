6c884bf2259eb19f464e0444f6993d3eea8b30b4
ComSecNinja <timo@comsec.ninja>
2017-05-28 18:49:13 +0300

    Add streamer

--- static/js/twitch-alert.js
+++ static/js/twitch-alert.js
@@ -1,6 +1,6 @@
 var streamersUpdate = function() {
     var key = "ph06xyfig2wp3z4qzz2mv6y49caq6un";
-    var users = ["MrHamsterOW", "r0n5k1", "TipiOW", "tombhaonair"];
+    var users = ["MrHamsterOW", "r0n5k1", "TipiOW", "karvarausku"];
     $(".twitch-alert").empty();
     users.forEach(function (user) {
         twitchInfo(user, key, function(data, err) {
